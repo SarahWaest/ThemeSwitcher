45705bafdefc8bc118b67fca3b3c285e9ab66b48
sarah <sarahkatrine@hotmail.com>
2020-12-18 02:24:12 +0100

    first commit

---
+++ app/src/main/java/com/example/themeswitcher/ThemeApplication.java
@@ -0,0 +1,10 @@
+package com.example.themeswitcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Application;
+import android.os.Bundle;
+
+public class ThemeApplication extends Application {
+    public static int currentPosition;
+}
